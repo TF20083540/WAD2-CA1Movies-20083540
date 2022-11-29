849fdb2c7a95f46913c6753d580c849248940375
TF20083540 <20083540@mail.wit.ie>
2022-11-29 04:09:14 +0000

    Added upcoming movies page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -11,6 +11,8 @@ export const getMovies = () => {
      throw error
   });
 };
+
+
   
 export const getMovie = (args) => {
    console.log(args)
@@ -71,4 +73,18 @@ export const getMovie = (args) => {
         // console.log(json.results);
         return json.results;
       });
+  };
+
+  export const getUpcomingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
   };
--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import SiteHeader from './components/siteHeader';
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage'
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage"; // Part 3 Exercise
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -31,6 +32,7 @@ const App = () => {
         <Routes>
           <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
           <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+          <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
